ace359db70b57c97f65111247c5d644bdc5af796
yugeeklab <yugeeklab@gmail.com>
2021-01-16 21:35:01 +0900

    day6

--- loader.s
+++ loader.s
@@ -5,6 +5,13 @@ FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+section .bss
+align 4                                     ; align at 4 bytes
+kernel_stack:                               ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
@@ -16,12 +23,10 @@ loader:                         ; the loader label (defined as entry point in li
 
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
+
+    ; The assembly code
+    extern fb_move_cursor   ; the function sum_of_three is defined elsewhere
+    push dword 5            ; arg1
+    call fb_move_cursor       ; call the function, the result will be in eax
 .loop:
     jmp .loop                   ; loop forever
-
-KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
-
-section .bss
-align 4                                     ; align at 4 bytes
-kernel_stack:                               ; label points to beginning of memory
-    resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
